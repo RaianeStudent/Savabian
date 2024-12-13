e0b5937408785baf4d96d27f209ceb39a6a093fa
RaianeEstudante <raiane@alunos.utfpr.edu.br>
2024-12-13 12:25:02 -0300

    Customizar Erros e ajustando app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const supplierRoutes = require('./routes/supplierRoutes');
+const cartRoutes = require('./routes/cartRoutes');
 
 const app = express();
 app.use(express.json());
+
+// Register routes
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/suppliers', supplierRoutes);
+app.use('/cart', cartRoutes);
+
+// Error middleware
+app.use(errorMiddleware);
+
 
 app.listen(3000, () => {
     console.log('Servidor ouvindo na porta 3000');
---
+++ middlewares/errorMiddleware.js
@@ -0,0 +1,5 @@
+module.exports = (err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || 'Erro interno do servidor.';
+    res.status(status).json({ message });
+  };
