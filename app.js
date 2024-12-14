eb9d99171438241b24630d9566ac5e48baa2ead4
diomenes23 <diomenes@alunos.utfpr.edu.br>
2024-12-14 11:12:53 -0300

    documentação

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json')
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const supplierRoutes = require('./routes/supplierRoutes');
 const cartRoutes = require('./routes/cartRoutes');
+const contactRoutes = require('./routes/contactRoutes');
+const homeRoutes = require('./routes/homeRoutes');
+const installRoutes = require('./routes/installRoutes');
+const { authorizeAdmin, authorizeUserOrAdmin } = require('./middlewares/authMiddleware');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+app.use('/contact', contactRoutes);
+app.use('/', homeRoutes);
+app.use('/install', installRoutes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const app = express();
 app.use(express.json());
 
-// Register routes
+
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/suppliers', supplierRoutes);
 app.use('/cart', cartRoutes);
 
-// Error middleware
 app.use(errorMiddleware);
 
 
